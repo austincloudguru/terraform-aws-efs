1d421b91d7974680f0e43fc9fbd078e315715d94
Mark Honomichl <mark@honomichl.net>
2023-01-31 15:43:04 -0600

    Fixing the version

--- examples/terratest/main.tf
+++ examples/terratest/main.tf
@@ -6,10 +6,12 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">= 0.12.6, < 1.4"
-
+  required_version = "~> 1.3.0"
   required_providers {
-    aws = ">= 2.68, < 4.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
 }
 
